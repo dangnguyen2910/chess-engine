9d97c9b9ec3a1a5b42c26ee376ed565cfd65909e
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2026-09-01 16:17:38 +0700

    test: test king pseudo-legal move generator.

--- tests/move_generator_test.cc
+++ tests/move_generator_test.cc
@@ -74,3 +74,51 @@ TEST(MoveGenerator, generate_black_knights_moves_pos_1) {
 
     ASSERT_EQ(moves, expected);
 }
+
+TEST(MoveGenerator, generate_white_king_moves_pos1) {
+    using enum Square;
+    Board board;
+    board.set_piece(Piece::KING, Color::WHITE, Square::A3);
+    board.set_piece(Piece::ROOK, Color::WHITE, Square::A2);
+    board.set_moveside(Color::WHITE);
+
+    MoveGenerator generator = MoveGenerator(board);
+    std::uint64_t moves = generator.generate_king_moves();
+    std::uint64_t expected = 0ULL;
+
+    expected |= to_bitboard(A4) | to_bitboard(B2) | to_bitboard(B3) | to_bitboard(B4);
+    ASSERT_EQ(moves, expected);
+}
+
+TEST(MoveGenerator, generate_white_king_moves_pos2) {
+    using enum Square;
+    Board board;
+    board.set_piece(Piece::KING, Color::WHITE, Square::D1);
+    board.set_piece(Piece::KING, Color::WHITE, Square::D8);
+    board.set_piece(Piece::KING, Color::WHITE, Square::H4);
+    board.set_moveside(Color::WHITE);
+
+    MoveGenerator generator = MoveGenerator(board);
+    std::uint64_t moves = generator.generate_king_moves();
+    std::uint64_t expected = 0ULL;
+
+    expected |= to_bitboard(C1) | to_bitboard(E1) | to_bitboard(C2) | to_bitboard(D2) | to_bitboard(E2);
+    expected |= to_bitboard(H3) | to_bitboard(H5) | to_bitboard(G3) | to_bitboard(G4) | to_bitboard(G5);
+    expected |= to_bitboard(C8) | to_bitboard(E8) | to_bitboard(C7) | to_bitboard(D7) | to_bitboard(E7);
+    ASSERT_EQ(moves, expected);
+}
+
+TEST(MoveGenerator, generate_white_king_moves_pos3) {
+    using enum Square;
+    Board board;
+    board.set_piece(Piece::KING, Color::WHITE, Square::E4);
+    board.set_moveside(Color::WHITE);
+
+    MoveGenerator generator = MoveGenerator(board);
+    std::uint64_t moves = generator.generate_king_moves();
+    std::uint64_t expected = 0ULL;
+
+    expected |= to_bitboard(D5) | to_bitboard(E5) | to_bitboard(F5) | to_bitboard(F4)
+             | to_bitboard(F3) | to_bitboard(E3) | to_bitboard(D3) | to_bitboard(D4);
+    ASSERT_EQ(moves, expected);
+}
