614beaf208b52b627851149d364d1916f6768173
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2026-09-19 15:41:43 +0700

    chore: add bishops getter function.

--- include/board.h
+++ include/board.h
@@ -33,11 +33,13 @@ class Board {
         std::uint64_t get_white_knights_pos() { return _white_knights_pos; }
         std::uint64_t get_white_king_pos() { return _white_king_pos; }
         std::uint64_t get_white_rooks_pos() { return _white_rooks_pos; }
+        std::uint64_t get_white_bishops_pos() { return _white_bishops_pos; }
 
         std::uint64_t get_black_pieces_pos();
         std::uint64_t get_black_knights_pos() { return _black_knights_pos; };
         std::uint64_t get_black_king_pos() { return _black_king_pos; }
         std::uint64_t get_black_rooks_pos() { return _black_rooks_pos; }
+        std::uint64_t get_black_bishops_pos() { return _black_bishops_pos; }
 
         bool is_white_move() { return _is_white_move; }
 
